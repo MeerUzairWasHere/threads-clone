de4e09bf671164aed5f030d6bcc4c4ea86ca6ac3
Meer Uzair <meer.uxair007@gmail.com>
2024-01-08 03:14:41 +0530

    create fetchPosts() server action

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import { fetchPosts } from "@/lib/actions/thread.actions";
+
+export default async function Home() {
+  const { posts } = await fetchPosts(1, 30);
+  console.log(posts);
   return (
     <>
-<h1 className="head-text text-left">Home</h1>      
+      <h1 className="head-text text-left">Home</h1>
+      <section className="mt-9 flex flex-col gap-10">
+        {posts.length === 0 ? (
+          <p className="no-result">No threads found! </p>
+        ) : (
+          <>
+            {posts.map((post) => (
+              <p className=" text-light-1">{post.text}</p>
+              //start here
+            ))}
+          </>
+        )}
+      </section>
     </>
   );
 }
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -28,3 +28,29 @@ export async function createThread({ text, author, communityId, path }: Params) 
         throw new Error(`Failed to create thread: ${error.message}`)
     }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    try {
+        connectToBD()
+        const skip = (pageNumber - 1) * pageSize
+        const postsQuery = Thread.find(
+            { parentId: { $in: [null, undefined] } }).sort(
+                { createdAt: "desc" }).skip(skip).limit(pageSize).populate({ path: "author", model: User }).populate({
+                    path: "children", populate: {
+                        path: "author",
+                        model: User,
+                        select: "_id name parentId image"
+                    }
+                })
+        const totalPostCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } })
+
+        const posts = await postsQuery.exec()
+        const isNext = totalPostCount > skip + posts.length
+
+        return { posts, isNext }
+    } catch (error: any) {
+        throw new Error(`Failed to fetch Posts: ${error.message}`)
+
+
+    }
+}
